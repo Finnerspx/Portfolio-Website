769cc352f34c6b7f93535818a166947e1b5cd51f
Finnerspx <finleypurnell98@gmail.com>
2019-11-24 14:13:53 +0000

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024',
+      'xl': '1280px',
+    },
     extend: {}
   },
   variants: {},
